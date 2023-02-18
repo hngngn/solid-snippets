b6606fa8cb25c3ecd2cc8adfd00ff4758098c591
hnggngn <rozephyrus@gmail.com>
2023-02-18 14:11:33 +0700

    Initial Commit

---
+++ src/main/kotlin/com/hngngn/solid/snippets/solidsnippets/SolidContext.kt
@@ -0,0 +1,11 @@
+package com.hngngn.solid.snippets.solidsnippets
+
+import com.intellij.codeInsight.template.TemplateActionContext
+import com.intellij.codeInsight.template.TemplateContextType
+
+class SolidContext private constructor() : TemplateContextType("SOLID", "Solid") {
+    override fun isInContext(templateActionContext: TemplateActionContext): Boolean {
+        val name = templateActionContext.file.name
+        return name.endsWith(".ts") || name.endsWith(".tsx")
+    }
+}
