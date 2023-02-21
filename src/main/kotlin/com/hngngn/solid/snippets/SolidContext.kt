2d5bb8c2c87af8c628a93c7a21d8c2e56b4de0df
hnggngn <rozephyrus@gmail.com>
2023-02-21 11:43:56 +0700

    feat: Support 2022.3
    - Update TemplateContextType
    - Void component with named export

--- src/main/kotlin/com/hngngn/solid/snippets/SolidContext.kt
+++ src/main/kotlin/com/hngngn/solid/snippets/SolidContext.kt
@@ -3,7 +3,7 @@ package com.hngngn.solid.snippets
 import com.intellij.codeInsight.template.TemplateActionContext
 import com.intellij.codeInsight.template.TemplateContextType
 
-class SolidContext private constructor() : TemplateContextType("SOLID", "Solid") {
+class SolidContext private constructor() : TemplateContextType("Solid") {
     override fun isInContext(templateActionContext: TemplateActionContext): Boolean {
         val name = templateActionContext.file.name
         return name.endsWith(".ts") || name.endsWith(".tsx")
